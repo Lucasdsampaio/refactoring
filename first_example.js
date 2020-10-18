eb54aff7737876b03220191ba27a62529b657daa
Lucas Sampaio <lucas.sampaio@grupozap.com>
2020-10-18 20:24:04 -0300

    rename the variables inside the new function

--- first_example.js
+++ first_example.js
@@ -56,10 +56,10 @@ function amountFor(aPerformance) {
     return result;
 }
 
-function volumeCreditsFor(perf) {
+function volumeCreditsFor(aPerformance) {
     let volumeCredits = 0
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
 }
 
 function statement(invoice) {

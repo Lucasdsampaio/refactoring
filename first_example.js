307075de622b479eb7643b3735f66870251722fe
Lucas Sampaio <lucas.sampaio@grupozap.com>
2020-10-19 21:07:29 -0300

    rename variables to convention

--- first_example.js
+++ first_example.js
@@ -77,19 +77,19 @@ function formatAsUSD(aNumber) {
 }
 
 function totalVolumeCredits() {
-    let volumeCredits = 0
+    let result = 0
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf)
+        result += volumeCreditsFor(perf)
     }
-    return volumeCredits
+    return result
 }
 
 function totalAmount() {
-    let totalAmount = 0
+    let result = 0
     for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf)
+        result += amountFor(perf)
     }
-    return totalAmount
+    return result
 }
 
 function statement(invoice, plays) {

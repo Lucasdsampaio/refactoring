98995ef71fb02d6babe8282957c86e5998de341f
Lucas Sampaio <lucas.sampaio@grupozap.com>
2021-04-28 22:10:10 -0300

    example extract variable

---
+++ extractVariable/extractVariable.js
@@ -0,0 +1,4 @@
+function price(order) {
+    //price is base price ­ quantity discount + shipping
+    return order.quantity * order.itemPrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.5 + Math.min(order.quantity * order.itemPrice * 0.01, 100)
+}

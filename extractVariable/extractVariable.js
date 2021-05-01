669e715f171444d8d1406c0808012873a47e4b75
Lucas Sampaio <lucas.sampaio@grupozap.com>
2021-05-01 18:35:22 -0300

    create variable

--- extractVariable/extractVariable.js
+++ extractVariable/extractVariable.js
@@ -1,4 +1,5 @@
 function price(order) {
     //price is base price ­ quantity discount + shipping
+    const basePrice = order.quantity * order.itemPrice
     return order.quantity * order.itemPrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.5 + Math.min(order.quantity * order.itemPrice * 0.01, 100)
 }

660f48a8d387886b5c9aa26a8c6013619a6cd387
Lucas Sampaio <lucas.sampaio@grupozap.com>
2021-05-01 18:40:05 -0300

    create shipping and remove unnecessary comment

--- extractVariable/extractVariable.js
+++ extractVariable/extractVariable.js
@@ -1,5 +1,6 @@
 function price(order) {
-    //price is base price ­ quantity discount + shipping
     const basePrice = order.quantity * order.itemPrice
-    return basePrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.5 + Math.min(basePrice * 0.01, 100)
+    const quantityDiscount =  Math.max(0, order.quantity - 500) * order.itemPrice * 0.5 
+    const shipping = Math.min(basePrice * 0.01, 100)
+    return basePrice - quantityDiscount + shipping
 }
